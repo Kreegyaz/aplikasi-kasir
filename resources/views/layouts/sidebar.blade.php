5158a767a8e313af9cf51d8f5c28f3b7dc2b3bc9
Kreegyaz <divad.dip.1610@gmail.com>
2025-01-21 20:06:54 +0700

    first commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,91 @@
+
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <!-- <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image"> -->
+            </div>
+            <div class="pull-left info">
+                <!-- <p>Alexander Pierce</p>
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a> -->
+            </div>
+        </div>
+        <!-- /.search form -->
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu" data-widget="tree">
+            <li>
+                <a href="{{ route('dashboard') }}">
+                    <i class="fa fa-dashboard"></i> <span>Home</span>
+                </a>
+            </li>
+
+            <li class="header">MASTER</li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-cube" aria-hidden="true"></i> <span>Kategori</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-book" aria-hidden="true"></i> <span>Produk</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-address-book-o" aria-hidden="true"></i> <span>Member</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-truck" aria-hidden="true"></i> <span>Supplier</span>
+                </a>
+            </li>
+            <li class="header">TRANSAKSI</li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-upload" aria-hidden="true"></i> <span>Pengeluaran</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-money" aria-hidden="true"></i> <span>Pembelian</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-download" aria-hidden="true"></i> <span>Penjualan</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-calculator" aria-hidden="true"></i> <span>Kalkulator Kasir</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-archive" aria-hidden="true"></i> <span>Riwayat Transaksi</span>
+                </a>
+            </li>
+            <li class="header">LAPORAN</li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-star" aria-hidden="true"></i> <span>Laporan</span>
+                </a>
+            </li>
+            <li class="header">SYSTEM</li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-user-o" aria-hidden="true"></i> <span>User</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-cog" aria-hidden="true"></i> <span>Pengaturan</span>
+                </a>
+            </li>
+        </ul>
+    </section>
+    <!-- /.sidebar -->
+</aside>
